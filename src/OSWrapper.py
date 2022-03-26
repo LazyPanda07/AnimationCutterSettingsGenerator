97a94363ca0d15faaf40870865e690767a7098a9
LazyPanda07 <semengricenko@gmail.com>
2022-03-26 22:38:46 +0500

    Add macOS icons

--- src/OSWrapper.py
+++ src/OSWrapper.py
@@ -2,38 +2,45 @@ import sys
 import platform
 import os
 from PyQt6.QtGui import QIcon
-from PyQt6.QtCore import QSize
 
 
 class OSWrapper:
 	__linux_sizes = [
-		QSize(8, 8),
-		QSize(16, 16),
-		QSize(20, 20),
-		QSize(22, 22),
-		QSize(24, 24),
-		QSize(28, 28),
-		QSize(32, 32),
-		QSize(36, 36),
-		QSize(40, 40),
-		QSize(42, 42),
-		QSize(48, 48),
-		QSize(64, 64),
-		QSize(72, 72),
-		QSize(96, 96),
-		QSize(128, 128),
-		QSize(192, 192),
-		QSize(256, 256),
-		QSize(512, 512)
+		"8x8",
+		"16x16",
+		"20x20",
+		"22x22",
+		"24x24",
+		"28x28",
+		"32x32",
+		"36x36",
+		"40x40",
+		"42x42",
+		"48x48",
+		"64x64",
+		"72x72",
+		"96x96",
+		"128x128",
+		"192x192",
+		"256x256",
+		"512x512"
+	]
+
+	__mac_os_sizes = [
+		"16x16",
+		"32x32",
+		"128x128",
+		"256x256",
+		"512x512"
 	]
 
 	__platform_icon_name = {
 		"Windows": "icon.ico",
-		"Darwin": "icon.icns",
+		"Darwin": "",
 		"Linux": ""
 	}
 
-	def __get_icon_path(self):
+	def __get_icon_path(self) -> str:
 		bundle_dir = getattr(sys, "_MEIPASS", os.path.dirname(__file__))
 		icon_path = os.path.abspath(os.path.join(bundle_dir, f"assets/{self.platform}/{OSWrapper.__platform_icon_name[self.platform]}"))
 
@@ -42,18 +49,24 @@ class OSWrapper:
 
 		return icon_path
 
+	def __init_icon(self, data: list[str], extension: str) -> QIcon:
+		app_icon = QIcon()
+		path_to_icon = self.__get_icon_path()
+
+		for size in data:
+			app_icon.addFile(path_to_icon.join(size).join(extension))
+
+		return app_icon
+
 	def __init__(self):
 		self.platform = platform.system()
 
 	def get_icon(self) -> QIcon:
-		if self.platform == "Windows" or self.platform == "Darwin":
+		if self.platform == "Windows":
 			return QIcon(self.__get_icon_path())
 
+		elif self.platform == "Darwin":
+			return self.__init_icon(OSWrapper.__mac_os_sizes, ".icns")
+
 		elif self.platform == "Linux":
-			app_icon = QIcon()
-			path_to_icon = self.__get_icon_path()
-
-			for size in OSWrapper.__linux_sizes:
-				app_icon.addFile(path_to_icon.join(size.width()).join('x').join(size.height()).join(".png"))
-
-			return app_icon
+			return self.__init_icon(OSWrapper.__linux_sizes, ".png")
